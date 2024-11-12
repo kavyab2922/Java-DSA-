495b74a4b8f48ca55a5833c15b0db9ac45b090f2
Kishore Kumar <kishorekumar@Kishores-MacBook-Air.local>
2024-11-12 16:25:04 +0530

    first commit

--- Pattern/star.java
+++ Pattern/star.java
@@ -1,7 +1,18 @@
 package Pattern;
+import java.util.Scanner;
 
 public class star{
     public static void main(String[] args){
-        System.out.println("hi");
+        System.out.print("Enter the the number of rows:");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*"+ " ");
+            }
+            System.out.println();
+        }
+        
     }
 }
